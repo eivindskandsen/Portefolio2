db0adb44662191d0bb6dd638f68bd3a88b4b3888
eivind <eivindskandsen@hotmail.no>
2021-04-26 14:21:40 +0200

    Hei

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from flask import Flask, redirect, url_for, render_template
+from flask import jsonify
 
 app = Flask(__name__)
 
+#app= flask.Flask(_name_)
+app.config["DEBUG"]= True
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+products= [
+    {
+        'id': 0,
+        'P_name': "Eple",
+        'bilde': "Et bilde"
+    }
+]
 @app.route('/products')
 def get_drinks():
-    return 'Hello there!'
+    return jsonify(products)
 
 
 if __name__ == '__main__':
