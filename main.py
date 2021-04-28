f34d55e1d3bd11f65a866d98c438d8d9edc53cbf
eivind <eivindskandsen@hotmail.no>
2021-04-28 15:08:32 +0200

    ett push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask import jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -7,10 +8,42 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
+my_db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="kirakira22",
+        database="potet"
+    )
+
+
+my_cursor = my_db.cursor()
+
+my_cursor.execute("select * from products")
+
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
+def connect_db():
+    my_db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="kirakira22",
+        database="potet"
+    )
+    print(my_db)
+
+    my_cursor = my_db.cursor()
+
+    my_cursor.execute("select * from products")
+
+    for x in my_cursor:
+        print(x)
+
+    my_cursor.execute("select navn from products")
+
+    for y in my_cursor:
+        print(y)
 
 products = [
     {
@@ -23,8 +56,14 @@ products = [
 
 @app.route('/products', methods=['GET'])
 def get_drinks():
-    return jsonify(products)
+    return render_template("products.html")
 
 
 if __name__ == '__main__':
+
+
+    connect_db()
+
     app.run(debug=True)
+
+
