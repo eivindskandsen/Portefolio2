550c6f8e14bc3dc6b1cd56897fe541bf13dd1103
eivind <eivindskandsen@hotmail.no>
2021-05-05 10:57:59 +0200

    index-html

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 from flask import jsonify
 import mysql.connector
+import flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="C:/Users/eivin/PycharmProjects/Portefolio2", static_url_path="")
 
 # app= flask.Flask(_name_)
 app.config["DEBUG"] = True
@@ -20,50 +21,28 @@ my_cursor = my_db.cursor()
 
 my_cursor.execute("select * from products")
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template("index.html")
-
-def connect_db():
-    my_db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="kirakira22",
-        database="potet"
-    )
-    print(my_db)
-
-    my_cursor = my_db.cursor()
-
-    my_cursor.execute("select * from products")
-
-    for x in my_cursor:
-        print(x)
-
-    my_cursor.execute("select navn from products")
-
-    for y in my_cursor:
-        print(y)
-
-products = [
-    {
-        'id': 0,
-        'P_name': "Eple",
-        'bilde': "Et bilde"
-    }
-]
+@app.route('/', defaults={'path': 'index.html'})
+#def index():
+@app.route('/<path>')
+def page(path):
+    print("Request recieved for {}".format(path))
+    return flask.send_from_directory('C:/Users/eivin/PycharmProjects/Portefolio2', path)
 
 
 @app.route('/products', methods=['GET'])
 def get_drinks():
-    return render_template("products.html")
+    #my_cursor.execute("SELECT * from products")
+    my_results= my_cursor.fetchall()
+    print(my_results)
+    return flask.jsonify(my_results)
+
+
+
 
 
 if __name__ == '__main__':
 
-
-    connect_db()
-
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
+    my_db.close()
 
 
