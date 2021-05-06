a3296f1a4f3e6df5de7639deb0076b91aae10063
eivind <eivindskandsen@hotmail.no>
2021-05-06 21:10:55 +0200

    hva skjer med projsektet nnå

--- sql/db.sql
+++ sql/db.sql
@@ -12,11 +12,11 @@ CREATE TABLE products (
 INSERT INTO products (name, price) VALUES
 ('linsesuppe', 100),
 ('ertesuppe', 125),
-('brokkolisuppe', 150)
+('brokkolisuppe', 150),
 ('brenneslesuppe', 75);
 
 CREATE USER 'anonymous' IDENTIFIED BY 'pikachoo';
 GRANT SELECT ON products TO 'ananymous';
 
 CREATE USER 'admin' IDENTIFIED BY '';
-GRANT * ON products TO 'admin';
+GRANT SELECT, INSERT ON products TO 'admin';
